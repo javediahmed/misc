26fc914dea3affc5e01afa93a9976dba9e951470
javediahmed <javed.ahmed@thisismetis.com>
2020-04-10 01:53:56 -0700

    update 2

--- airbnb/test.py
+++ airbnb/test.py
@@ -31,7 +31,7 @@ def main(url=URL, output_dir=OUTPUT_DEST, unzip=True):
     links = find_links(url)
     datalinks = parse_links(links, datatypes=['csv', 'json'])
     print("Writing files:")
-    for link in datalinks[:5]:
+    for link in datalinks:
         link_filename = '_'.join(link.split('/')[3:])
         print(f'{link_filename}')
         urlretrieve(link, output_dir + link_filename)
@@ -45,20 +45,3 @@ if __name__ == '__main__':
     print('End:   ', str(end)[:-7])    
     print(f'Time:   {round(time.total_seconds()/60, 1)} minutes')    
     
-
-
-#from bsparse import get_soup
-#response = requests.get(url)
-#page = response.text
-#soup = BeautifulSoup(page, "lxml")
-#soup2 = get_soup(url)
-#links = soup.findAll('a')
-#l1 = links[6]
-#l2 = links[8]
-#dlinks = [x for x in links if 'csv' in str(x) or 'json' in str(x)]
-#dhttps = [x.attrs['href'] for x in dlinks]
-#hlinks = [x.attrs for x in links]
-#testurl = dhttps[testlinks]
-#filename = '_'.join(testurl.split('/')[3:])
-#urlretrieve(testurl, output_dir + filename)
-
