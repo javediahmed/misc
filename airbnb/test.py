30f2a0ada0fc88a5a3bca8e3905a2de77904feac
javediahmed <javed.ahmed@thisismetis.com>
2020-04-10 06:02:10 -0700

    update:

--- airbnb/test.py
+++ airbnb/test.py
@@ -10,6 +10,7 @@ from datetime import datetime
 
 URL = 'http://insideairbnb.com/get-the-data.html'
 OUTPUT_DEST = 'data/'
+START_FILE = 'greece_crete_crete_2019-02-16_data_listings.csv.gz'
 
 def find_links(url):
     response = requests.get(URL)
@@ -27,18 +28,33 @@ def parse_links(linklist, datatypes=['csv'], url_only=True):
                     datalinks.append(link)
     return(datalinks)            
 
-def main(url=URL, output_dir=OUTPUT_DEST, unzip=True):
+def main(url=URL, output_dir=OUTPUT_DEST, unzip=True, start_file=START_FILE):
     links = find_links(url)
     datalinks = parse_links(links, datatypes=['csv', 'json'])
+    link_filenames = ['_'.join(link.split('/')[3:]) for link in datalinks]
+    if start_file is not None and start_file in link_filenames:
+        start_num = link_filenames.index(start_file)
+    else: start_num = 0
+    link_targets = datalinks[start_num:]
+    link_errors = []
+    #breakpoint()
     print("Writing files:")
-    for link in datalinks:
-        link_filename = '_'.join(link.split('/')[3:])
+    for l, link in enumerate(links_targets):
+        #link_filename = '_'.join(link.split('/')[3:])
         print(f'{link_filename}')
-        urlretrieve(link, output_dir + link_filename)
+        try:
+            urlretrieve(link, output_dir + link_filename)           
+        except HTTPError:
+            print('** HTTP Error: {link_filename}')
+            link_errors.append(link_filename)
+    print(f'Files with errors ({len(link_errors)} of {len(datalinks_target)}):')
+    for error in link_errors:
+        print(error)
       
 if __name__ == '__main__':
     start = datetime.now()
     main()
+    
     end = datetime.now()
     time = end - start
     print('Start: ', str(start)[:-7])
