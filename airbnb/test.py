37572bd7825c973b3fdfc4b3ba2505a8cd2783cc
javediahmed <javed.ahmed@thisismetis.com>
2020-04-11 16:08:29 -0700

    update test

--- airbnb/test.py
+++ airbnb/test.py
@@ -10,8 +10,16 @@ os.chdir(data_path)
 
 seafiles = glob.glob('*seattle*agg.pkl')
 dfs = []
-
+names = []
 for sfile in seafiles:
-    dfs.append(pd.read_pickle(sfile))
+    newdf = pd.read_pickle(sfile)
+    stamp = str(newdf.index[0].date())
+    newdf['stamp'] = stamp
+    newdf.columns = ['_'.join(col).strip() for col in newdf.columns.values]
+    dfs.append(newdf)
+    names.append(stamp)
+
+avail = pd.DataFrame(columns=names)
+
 
 #breakpoint()
