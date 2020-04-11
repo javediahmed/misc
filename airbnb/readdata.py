9f9f5eaa6b61a39dce0e5c4d95cb476767dcfa25
javediahmed <javed.ahmed@thisismetis.com>
2020-04-11 14:20:09 -0700

    udpate

--- airbnb/readdata.py
+++ airbnb/readdata.py
@@ -8,7 +8,32 @@ outfile = 'us_df.pkl'
 outfile_test = 'us_df_int.pkl'
 error_file = 'errors.pkl'
 
-def read_files(file_list):
+def agg_calendar_file(df, datevar='date', idvar='listing_id'):
+    df.groupby('date')
+
+def read_calendar_file(fname, dpath=data_path, agg=False, output=False):
+    print(fname)
+    df = pd.read_csv(dpath + fname)
+    df['fullname'] = fname
+    df['available'] = df.available == 't'
+    df['price'] = df['price'].str.replace('$', '')
+    df['price'] = df['price'].str.replace(',', '')
+    df['price'] = df['price'].astype(float)
+    if 'adjusted_price' in df.columns:
+        df['adjusted_price'] = df['adjusted_price'].str.replace('$', '')
+        df['adjusted_price'] = df['adjusted_price'].str.replace(',', '')
+        df['adjusted_price'] = df['adjusted_price'].astype(float)
+    
+    if output:
+#        output_name = dpath + fname.split('.')[0] + '.pkl'
+        output_name = 'sea2.pkl'
+        print('Writing output file: ', output_name)
+#        pickle.dump(df, open(output_name, 'wb'), protocol=4)
+        df.to_pickle(output_name, protocol=4)
+    return df
+
+
+def read_files(file_list, output=False, output_file='test.pkl'):
     errors, output_df = [], pd.DataFrame()
     for fname in file_list:
         print(fname)
@@ -18,33 +43,49 @@ def read_files(file_list):
             output_df = pd.concat([output_df, df])
         except EOFError:
             errors.append(fname)
+    if output:
+        print('Writing output file') 
+        pickle.dump(output_df, open(output_file, 'wb'), protocol=4)        
     return output_df.drop_duplicates(), errors
 
-def read_calendar_file(fname, dpath=data_path):
-    print(fname)
-    df = pd.read_csv(dpath + fname)
-    df['fullname'] = fname
-    df['price'] = df['price'].str.replace('$', '')
-    df['price'] = df['price'].str.replace(',', '')
-    df['price'] = df['price'].astype(float)
-    df['available'] = df.available == 't'
-    return df
 
 lists = pickle.load(open(data_path + infile, 'rb'))
 csv_files, csv_errors, csv_lists, json_files, json_errors, json_lists = lists
 us_files = csv_files[csv_files.country == 'united-states']
 us_calendar_files = [x for x in csv_lists[0] if 'united-states' in x]
 us_calendar_data = csv_files[[i in us_calendar_files for i in csv_files.fullname]]
+state_calendar_files, city_calendar_files = [], []
+states = list(us_calendar_data.state.unique())
+cities = list(us_calendar_data.city.unique())
+for s in states:
+    files = list(us_calendar_data[us_calendar_data.state == s]['fullname'])
+    state_calendar_files.append(files)
 
+for c in cities:
+    files = list(us_calendar_data[us_calendar_data.city == c]['fullname'])
+    city_calendar_files.append(files)
+
+city_files_dict = dict(zip(cities, city_calendar_files))
+   
+def main():
+#    os.chdir(data_path)
+#    breakpoint()
+#    df, er = read_files(city_files_dict['seattle'], output=True, output_file='seattle.pkl')
+#    print('df shape: ', df.shape)
+#    print('err:', er)
+#    breakpoint()
+    sea = read_calendar_file(city_files_dict['seattle'][-1], dpath=data_path, output=True)  
+                             
 if __name__=='__main__':
-    dfs, errors = [], []
-    for i, fname in enumerate(us_calendar_files):
-        try:
-            dfs.append(read_calendar_file(fname))
-        except:
-            errors.append(fname)
-        if i % 100 == 0:
-            pickle.dump(dfs, open(data_path + outfile_test, 'wb'))
-    df_us = pd.concat(dfs)
-    df_us.to_pickle(data_path + outfile)
-    pickle.dump(errors, open(error_file, 'wb'))
+    main()
+
+
+
+
+#cities ['los-angeles', 'oakland', 'pacific-grove', 'san-diego',
+#       'san-francisco', 'san-mateo-county', 'santa-clara-county',
+#       'santa-cruz-county', 'denver', 'washington-dc', 'broward-county',
+#       'hawaii', 'chicago', 'new-orleans', 'boston', 'cambridge',
+#       'twin-cities-msa', 'asheville', 'jersey-city', 'clark-county-nv',
+#       'new-york-city', 'columbus', 'portland', 'salem-or',
+#       'rhode-island', 'nashville', 'austin', 'seattle']
--- airbnb/test.py
+++ airbnb/test.py
@@ -5,14 +5,7 @@ Run download.py, metadata.py first
 import glob, pandas as pd, os, pickle
 from metadata import outfile as infile, data_path
 
-csv_files, csv_errors, csv_lists, json_files, json_errors, json_lists = pickle.load(open(data_path + infile, 'rb'))
-us_files = csv_files[csv_files.country == 'united-states']
-us_calendar_files = [x for x in csv_lists[0] if 'united-states' in x]
+df = pd.read_pickle('sea.pkl')
 
-us_calendar_data = csv_files[[i in us_calendar_files for i in csv_files.fullname]]
-
-usefile = 'united-states_ny_new-york-city_2015-01-01_data_calendar.csv.gz'
-
-from readdata 
 
 breakpoint()
