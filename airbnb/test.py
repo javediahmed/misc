355fdd1b1f48563fabe4c7b9ea76b2beb3cd44df
unknown <javed.ahmed@thisismetis.com>
2020-04-13 23:25:29 -0700

    update

--- airbnb/test.py
+++ airbnb/test.py
@@ -3,14 +3,13 @@ Run download.py, metadata.py first
 """
 
 import glob, pandas as pd, os, pickle, matplotlib.pyplot as plt, numpy as np
-from metadata import outfile as infile, data_path
 
 
-curdir = os.getcwd()
-curdir = 'Z:/files/misc/airbnb/'
-os.chdir(data_path)
-
-readfiles = glob.glob('*seattle*agg.pkl')
+DATA_PATH = './data/'
+OUTPATH = './output/'
+CITIES = ('seattle', 'los-angeles', 'washington', 'san-francisco')
+#CITIES = ('seattle',)
+NUM_LINES = 4
 
 def combine(file_list, maxdays = 360):
     dfs, names = [], []
@@ -24,15 +23,29 @@ def combine(file_list, maxdays = 360):
         dfs.append(newdf.drop(columns='stampdate'))
     return dfs
 
-dfs = combine(readfiles)
-
-alldata = pd.concat(dfs[2:])
-allwide = alldata.reset_index().set_index(['date','stamp']).unstack()
-pldata = pd.concat([i[5:-1] for i in dfs[20:]])
-plwide = pldata.reset_index().set_index(['date','stamp']).unstack()
-plwide.rolling(1).mean().available_mean.plot(figsize=(20, 20), linewidth=2)
-plwide.rolling(7).mean().available_mean.plot(figsize=(20, 20), linewidth=2)
-plwide.rolling(1).mean().price_mean.plot(figsize=(20, 20), linewidth=2)
-plwide.rolling(7).mean().price_mean.plot(figsize=(20, 20), linewidth=2)
-
-plt.savefig('test.png')
+def main():
+    curdir = os.getcwd()
+    print(f'Current directory: {curdir}') 
+    for city in CITIES:
+        readfiles = glob.glob(DATA_PATH + f'*{city}*agg.pkl')
+        print(f'{len(readfiles)} files found for {city}')
+        dfs = combine(readfiles)
+        alldata = pd.concat(dfs[2:])
+        allwide = alldata.reset_index().set_index(['date','stamp']).unstack()
+#        print(allwide.head())
+#        breakpoint()
+        pldata = pd.concat([i[5:-1] for i in dfs[-NUM_LINES:]])
+#        breakpoint()
+        plwide = pldata.reset_index().set_index(['date','stamp']).unstack()
+        plwide.rolling(1).mean().available_mean.plot(figsize=(20, 20), linewidth=3)
+        print(f'Saving plots for {city}')
+        plt.savefig(OUTPATH + f'{city}_avail.png')
+        plwide.rolling(7).mean().available_mean.plot(figsize=(20, 20), linewidth=3)
+        plt.savefig(OUTPATH + f'{city}_avail_roll7.png')
+        plwide.rolling(1).mean().price_mean.plot(figsize=(20, 20), linewidth=3)
+        plt.savefig(OUTPATH + f'{city}_price.png')
+        plwide.rolling(7).mean().price_mean.plot(figsize=(20, 20), linewidth=3)
+        plt.savefig(OUTPATH + f'{city}_price_roll7.png')
+    
+if __name__=='__main__':
+    main()
