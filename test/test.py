6bc9c7723aa5b26d6facc06bb94de60bdd584f15
aic dev <javed.ahmed@outlook.com>
2022-06-03 16:10:04 -0700

    test

--- test/test.py
+++ test/test.py
@@ -1,17 +1,18 @@
-def get_first_name(full_name):
-    return full_name.split(" ")[0]
+''' test_template.py start template'''
 
-fallback_name = {
-    "first_name": "UserFirstName",
-    "last_name": "UserLastName"
-}
+INPUT_FILES = ('input_filename.txt', )
+OUTPUT_FILES = ('output_filename.txt', )
 
-raw_name = input("Please enter your name: ")
-print('raw name:', raw_name, 'type: ', type(raw_name))
-first_name = get_first_name(raw_name)
-
-# If the user didn't type anything in, use the fallback name
-if not first_name:
-    first_name = get_first_name(fallback_name)
-
-print(f"Hi, {first_name}!")
+class ExerciseSolution():
+    '''basic setup to run multiple steps'''
+    def __init__(self, input_files, input_function=read_data):
+        self.input_files=input_files
+        self.data = {name:input_function(input_file) for input_file in input_files}
+        print("len(self.data): ", len(self.data)
+    def question_1():
+        print('Question 1: ')
+    def question_2():
+        print('Question 2: ')
+    
+print(type(INPUT_FILES))
+# ExerciseSolution(INPUT_FILES)
