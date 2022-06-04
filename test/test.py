69dadfc855139184b157915487b5c3d3e50dd558
aic dev <javed.ahmed@outlook.com>
2022-06-03 18:20:17 -0700

    test

--- test/test.py
+++ test/test.py
@@ -1,18 +1,26 @@
 ''' test_template.py start template'''
 
-INPUT_FILES = ('input_filename.txt', )
-OUTPUT_FILES = ('output_filename.txt', )
+IN_FILES = ('./in1.txt', './in2.txt')
+OUT_FILES = ('output_filename.txt', )
 
-class ExerciseSolution():
-    '''basic setup to run multiple steps'''
-    def __init__(self, input_files, input_function=read_data):
-        self.input_files=input_files
-        self.data = {name:input_function(input_file) for input_file in input_files}
-        print("len(self.data): ", len(self.data)
-    def question_1():
-        print('Question 1: ')
-    def question_2():
-        print('Question 2: ')
-    
-print(type(INPUT_FILES))
-# ExerciseSolution(INPUT_FILES)
+class Exercise():
+    '''basic setup to run multiple step exercise'''
+    def __init__(self, in_files=None, out_files=None):
+        self.in_files = in_files
+        self.out_files = out_files
+        elif in_files:
+            self.data = {in_file:reader(in_file) for in_file in in_files}
+    with open(out_file, 'r') as data_file:
+        input_data = (input_record.strip('\n') for input_record in input_file)
+
+class Response(Exercise):
+    '''container for results'''
+    def __init__(self):
+       super.__init__()
+
+Response(IN_FILES)
+
+
+
+       
+           
